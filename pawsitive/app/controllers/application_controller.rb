3900ac729b58e378af661ea6decb80e4eee7a4db
lincoln gibson <lincolngibson7@gmail.com>
2024-05-12 14:50:14 -0300

    Add additional fields to devise controller to permit database entry.

--- pawsitive/app/controllers/application_controller.rb
+++ pawsitive/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # This method is used to add additional fields to the Devise registration form
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role, :first_name, :last_name,:photo, :price, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:role, :first_name, :last_name,:photo, :price,  :email, :password, :password_confirmation, :current_password])
+  end
 end
