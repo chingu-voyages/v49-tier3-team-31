46559a91962f0fb712f99823466b7ac9c5e34f8b
lincoln gibson <lincolngibson7@gmail.com>
2024-05-15 18:59:19 -0300

    create the booking controller with the create and new action to facilitate booking creation.

--- pawsitive/app/controllers/bookings_controller.rb
+++ pawsitive/app/controllers/bookings_controller.rb
@@ -1,23 +1,29 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def new
     @booking = Booking.new
+    @services = Service.all
+    puts @services.inspect
   end
 
   def create
     @booking = Booking.new(booking_params)
+    @services = Service.all
+    @booking.user_id = current_user.id
+
     if @booking.save
-      redirect_to root_path, flash[:notice] 'Booking was successfully created.'
+      redirect_to user_path(current_user), notice: 'Booking was successfully created.'
     else
+      flash.now[:alert] = @booking.errors.full_messages.to_sentence
       render :new
-      flash[:alert] = 'Booking was not created.'
     end
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:user_id, :service_id, :num_of_pets, :start_date, :end_date, :start_time, :end_time, :phone_number, :message, :recieve_updates)
+    params.require(:booking).permit(:service_id, :num_of_pets, :start_date, :end_date, :start_time, :end_time, :phone_number, :message, :recieve_updates)
   end
 end
+
