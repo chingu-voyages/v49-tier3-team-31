696fe2d977cb6ed23956987e1eea973ae6e02716
lincoln gibson <lincolngibson7@gmail.com>
2024-05-22 22:53:20 -0300

    set the notification count in the navbar.

--- pawsitive/app/controllers/application_controller.rb
+++ pawsitive/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_unread_notifications_count
 
   protected
 
@@ -11,5 +12,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :role])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :current_password,:bio, :role, :price, :photos, :avatar])
+  end
+  
+  def set_unread_notifications_count
+    if user_signed_in?
+      @unread_notifications_count = current_user.notifications.unread.count
+    end
   end
 end
