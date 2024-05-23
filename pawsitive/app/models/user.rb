3d7a41a32e796ef64f54cf7c481870e2f964ced4
gladdy17 <gladston.bosco@gmail.com>
2024-05-23 17:41:34 -0600

    removed the migration

--- pawsitive/app/models/user.rb
+++ pawsitive/app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
 
   has_many_attached :photos, service: :amazon, dependent: :destroy
   has_one_attached :avatar, service: :amazon, dependent: :destroy
-  has_one :account
 
   has_many :services, foreign_key: "member_id"
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :role, presence: true
+  validates :address, presence: true
   validates :bio, length: { maximum: 500 }
   enum role: { user: 0, member: 1 }
 
