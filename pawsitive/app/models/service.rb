6cc67c995db5850f2b17aa36df3651f5fdf98f93
lincoln gibson <lincolngibson7@gmail.com>
2024-05-25 12:23:10 -0300

    update validation.

--- pawsitive/app/models/service.rb
+++ pawsitive/app/models/service.rb
@@ -2,16 +2,17 @@ class Service < ApplicationRecord
   belongs_to :member, class_name: 'User', foreign_key: 'member_id'
   has_many :bookings, dependent: :destroy
 
-  validates :service_type, presence: true, inclusion: { in: %w[Dog\ Sitting Dog\ Boarding Dog] }
-  validates :description, presence: true, length: { minimum: 100 }
+  validates :service_type, presence: true, inclusion: { in: ["Day Care", "Boarding", "Sitting"] }
+  validates :description, presence: true, length: { minimum: 10 , maximum: 500}
   validates :availability, presence: true
   validates :price, presence: true
   validates :size, presence: true
+  validates :member_id, presence: true
 
-  scope :boarding, -> { where(service_type: "Dog Boarding") }
+  scope :boarding, -> { where(service_type: "Boarding") }
 
   def self.filtered(filters)
-    return Service.where(service_type: "Dog Boarding") if filters.nil?
+    return Service.where(service_type: "Boarding") if filters.nil?
     Service.where(service_type: filters[:service_type])
       .where("price -> 'base_rate' BETWEEN ? AND ?", filters[:price][:min], filters[:price][:max])
   end
