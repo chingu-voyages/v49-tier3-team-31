b07b568a67a17a8910ff4308ee541ad4e1548c96
lincoln gibson <lincolngibson7@gmail.com>
2024-05-25 12:14:05 -0300

    add new columns to devise sanitizer.

--- pawsitive/app/controllers/application_controller.rb
+++ pawsitive/app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   # This method is used to add additional fields to the Devise registration form
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :role])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :current_password,:bio, :role, :price, :photos, :avatar, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :current_password,:bio, :role, :price, :photos, :avatar, :address, :country, :city, :state, :zip, :birthday])
   end
   
   def set_unread_notifications_count
