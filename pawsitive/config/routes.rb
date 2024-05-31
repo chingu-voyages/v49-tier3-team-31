9c979b6e2eca12f936c6d29d4772a735fe66f6c0
lincoln gibson <lincolngibson7@gmail.com>
2024-05-31 16:01:20 -0300

    remove unnecessary re routing.

--- pawsitive/config/routes.rb
+++ pawsitive/config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: "users/registrations"
-  }
-
+  devise_for :users
 
   resources :users, only: [] do
     get :profile, on: :collection
