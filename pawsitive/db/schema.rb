93971a3eda779af32beeb7171e15070f1bc3bb8d
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-06-02 13:40:28 -0600

    Run migrations

--- pawsitive/db/schema.rb
+++ pawsitive/db/schema.rb
@@ -72,15 +72,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_26_233747) do
   end
 
   create_table "messages", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "sender_id", null: false
-    t.uuid "receiver_id", null: false
-    t.text "content", null: false
+    t.uuid "sender_id"
+    t.uuid "receiver_id"
+    t.text "content"
     t.uuid "booking_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["booking_id"], name: "index_messages_on_booking_id"
-    t.index ["receiver_id"], name: "index_messages_on_receiver_id"
-    t.index ["sender_id"], name: "index_messages_on_sender_id"
   end
 
   create_table "notifications", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -132,7 +130,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_26_233747) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "accounts", "users"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "availabilities", "services"
