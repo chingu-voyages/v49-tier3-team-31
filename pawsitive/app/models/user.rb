e93bd90966b55b41274418c6e6c0854d04ab0bb8
lincoln gibson <lincolngibson7@gmail.com>
2024-06-05 11:40:54 -0300

    rid validation

--- pawsitive/app/models/user.rb
+++ pawsitive/app/models/user.rb
@@ -18,11 +18,11 @@ class User < ApplicationRecord
   validates :bio, length: { maximum: 500 }
   enum role: { user: 0, member: 1 }
 
-  validates :country, presence: true
-  validates :state, inclusion: { in: -> (record) { record.states}, allow_blank: true}
-  validates :state, presence: { if: -> (record) { record.states.present?}}
-  validates :city, inclusion: { in: -> (record) { record.cities}, allow_blank: true }
-  validates :city, presence: { if: -> (record) { record.cities.present?}}
+  # validates :country, presence: true
+  # validates :state, inclusion: { in: -> (record) { record.states}, allow_blank: true}
+  # validates :state, presence: { if: -> (record) { record.states.present?}}
+  # validates :city, inclusion: { in: -> (record) { record.cities}, allow_blank: true }
+  # validates :city, presence: { if: -> (record) { record.cities.present?}}
 
   after_commit :add_default_avatar, on: %i[create update]
 
