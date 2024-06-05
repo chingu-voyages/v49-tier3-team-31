0cf41a3363419545190cce2b05a8c5b1a3f28f7a
lincoln gibson <lincolngibson7@gmail.com>
2024-06-05 11:59:14 -0300

    check navbar error

--- pawsitive/app/controllers/application_controller.rb
+++ pawsitive/app/controllers/application_controller.rb
@@ -8,8 +8,6 @@ class ApplicationController < ActionController::Base
     profile_users_path(resource) # Modify this according to your routes
   end
 
-
-
   # This method is used to add additional fields to the Devise registration form
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :role])
