09416e7968f9139e77ed84226997392f6ac62349
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-06-09 19:17:03 -0600

    Style home to be more responsive

--- pawsitive/app/javascript/controllers/filters_controller.js
+++ pawsitive/app/javascript/controllers/filters_controller.js
@@ -2,40 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="filters"
 export default class extends Controller {
+  static targets = ["hidable"];
+
   initialize() {
-    const slider = new RangeSlider('.range-slider', {
-      values: [1, 250],
-      min: 1,
-      max: 250,
-      pointRadius: 8
-    });
-
-    this.lowEle = document.querySelector(".range-low");
-    this.highEle = document.querySelector(".range-high");
-    this.lowInput = document.getElementById("filters_price_min");
-    this.highInput = document.getElementById("filters_price_max");
-    
-    slider.onChange(values => {
-      this.lowEle.textContent = `$${values[0]}`;
-      this.highEle.textContent = `$${values[1]}`;
-      this.lowInput.value = values[0];
-      this.highInput.value = values[1];
-      this.lowInput.form.requestSubmit();
-    });
-
     const location = document.getElementsByName("filters[location]")[0];
-    location.addEventListener("keyup", e => {
-      if (e.key === "Enter") e.target.form.requestSubmit();
-    });
+    if (window.screen.width < 768) {
+      location.addEventListener("blur", e => {
+        e.target.form.requestSubmit();
+      });
+    } else {
+      location.addEventListener("keyup", e => {
+        if (e.key === "Enter") e.target.form.requestSubmit();
+      });
+    }
   }
 
-  reset() {
-    document.querySelector(".range-slider").textContent = "";
-    this.initialize();
-    this.lowEle.textContent = "$1";
-    this.highEle.textContent = "$250";
-    this.lowInput.value = 1;
-    this.highInput.value = 250;
-    this.lowInput.form.requestSubmit();
+  toggle(e) {
+    e.preventDefault();
+    this.hidableTargets.forEach(ele => {
+      ele.classList.toggle("hidden");
+    });
   }
 }
---
+++ pawsitive/app/javascript/controllers/slider_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="slider"
+export default class extends Controller {
+  initialize() {
+    const slider = new RangeSlider('.range-slider', {
+      values: [1, 250],
+      min: 1,
+      max: 250,
+      pointRadius: 8
+    });
+
+    this.lowEle = document.querySelector(".range-low");
+    this.highEle = document.querySelector(".range-high");
+    this.lowInput = document.getElementById("filters_price_min");
+    this.highInput = document.getElementById("filters_price_max");
+    
+    slider.onChange(values => {
+      this.lowEle.textContent = `$${values[0]}`;
+      this.highEle.textContent = `$${values[1]}`;
+      this.lowInput.value = values[0];
+      this.highInput.value = values[1];
+      this.lowInput.form.requestSubmit();
+    });
+  }
+
+  reset() {
+    document.querySelector(".range-slider").textContent = "";
+    this.initialize();
+    this.lowEle.textContent = "$1";
+    this.highEle.textContent = "$250";
+    this.lowInput.value = 1;
+    this.highInput.value = 250;
+    this.lowInput.form.requestSubmit();
+  }
+}
