3285b998e363b5a39350ba5d3e36fccf8a338473
lincoln gibson <lincolngibson7@gmail.com>
2024-06-11 22:11:37 -0300

    fix submission form, model and controller

--- pawsitive/app/controllers/reviews_controller.rb
+++ pawsitive/app/controllers/reviews_controller.rb
@@ -1,34 +1,30 @@
 class ReviewsController < ApplicationController
   before_action :find_service, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def new
-    @review = Review.new
-    # @review = @service.reviews.build
+    @review = @service.reviews.build
   end
 
   def create
-    @service = Service.find(params[:id])
-    @review = @service.reviews.create(review_params)
-    @review.user_id = current_user.id
+    @review = @service.reviews.build(review_params)
+    @review.user = current_user
+
     if @review.save
-      redirect_to service_path(@service)
+      redirect_to service_path(@service), notice: 'Review successfully created.'
     else
-        render 'new'
-        @review.errors.full_messages
+      flash[:alert] = @review.errors.full_messages.to_sentence
+      render 'new'
     end
   end
 
   private
 
   def find_service
-    @service = Service.find(params[:id])
+    @service = Service.find(params[:service_id])
   end
 
   def review_params
-    params.require(:review).permit(:rating, :comment, :user_id)
+    params.require(:review).permit(:rating, :comment)
   end
-
-
-
-
 end
--- pawsitive/app/models/review.rb
+++ pawsitive/app/models/review.rb
@@ -4,7 +4,6 @@ class Review < ApplicationRecord
 
   validates :rating, presence: true
   validates :comment, presence: true
-
-
-
+  validates :service_id, presence: true
+  validates :user_id, presence: true
 end
