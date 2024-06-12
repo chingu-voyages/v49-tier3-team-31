12e849ed96351b2c5d06f317fb870eee68c76a5a
lincoln gibson <lincolngibson7@gmail.com>
2024-06-11 22:08:34 -0300

    edit original migration.

--- pawsitive/db/migrate/20240608124634_create_reviews.rb
+++ pawsitive/db/migrate/20240608124634_create_reviews.rb
@@ -1,6 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[7.1]
   def change
     create_table :reviews, id: :uuid do |t|
+      t.references :service, null: false, foreign_key: true, type: :uuid
+      t.references :user, null: false, foreign_key: true, type: :uuid
       t.integer :rating
       t.text :comment
 
